0440a6af6bffd79e44eb9a5fad14689c70781988
Ray Walters <ray@namely.com>
2016-06-19 16:32:59 -0400

    Add logging and clean up generators

--- lib/generators/mission.rb
+++ lib/generators/mission.rb
@@ -1,10 +1,16 @@
 module Generators
   class Mission
-    def call(company_name:, mission_name:)
+    def call(company_name: "Namely", mission_name: "Default Mission Name")
+      GRPC.logger.info("-"*20)
+      GRPC.logger.info(self.class.name)
+      GRPC.logger.info("Company Name: #{company_name}")
+      GRPC.logger.info("Mission Name: #{mission_name}")
+      GRPC.logger.info("-"*20)
+
       name_length = SecureRandom.random_number(9) + 1
       id = SecureRandom.random_number(900) + 100
 
-      Mission.new(
+      ::Mission.new(
         id: id,
         company_name: "#{company_name}_#{id}",
         client_id: name_length**2,
