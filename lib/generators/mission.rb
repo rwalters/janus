168d324cba9ed2d7b60c048d9fccf3fe71d83da8
Ray Walters <ray@namely.com>
2016-06-19 17:03:44 -0400

    Fixes for the generators after rubocop

--- lib/generators/mission.rb
+++ lib/generators/mission.rb
@@ -1,20 +1,14 @@
 module Generators
+  # Create a *protobuf* _Mission_ object for gRPC
   class Mission
-    def call(company_name: "Namely", mission_name: "Default Mission Name")
-      GRPC.logger.info("-"*20)
-      GRPC.logger.info(self.class.name)
-      GRPC.logger.info("Company Name: #{company_name}")
-      GRPC.logger.info("Mission Name: #{mission_name}")
-      GRPC.logger.info("-"*20)
-
-      name_length = SecureRandom.random_number(9) + 1
+    def call(company_name: 'Namely', mission_name: 'Default Mission Name')
       id = SecureRandom.random_number(900) + 100
 
       ::Mission.new(
         id: id,
         company_name: "#{company_name}_#{id}",
-        client_id: name_length**2,
-        mission_name: mission_name*name_length
+        client_id: (id % 17),
+        mission_name: mission_name * (id % 9)
       )
     end
   end
--- lib/generators/mission_list.rb
+++ lib/generators/mission_list.rb
@@ -1,20 +1,11 @@
 module Generators
+  # Create a *protobuf* _MissionList_ object for gRPC
   class MissionList
     def call(list_length: (SecureRandom.random_number(9) + 1))
-      GRPC.logger.info("-"*20)
-      GRPC.logger.info(self.class.name)
-      GRPC.logger.info("list_length: #{list_length}")
-
-      mission_ary =
-        list_length.times.map do
+      ::MissionList.new(
+        missions: Array.new(list_length) do
           Generators::Mission.new.call(mission_name: 'Gen Mission List')
         end
-
-      GRPC.logger.info(mission_ary.inspect)
-      GRPC.logger.info("-"*20)
-
-      ::MissionList.new(
-        missions: mission_ary
       )
     end
   end
