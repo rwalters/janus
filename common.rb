2d878aa49b258c0d588aad056be866bd35939210
Ray Walters <ray@namely.com>
2016-06-19 16:32:35 -0400

    Update common require file with generators

--- common.rb
+++ common.rb
@@ -1,10 +1,16 @@
 lib_dir = File.join(File.expand_path(File.dirname(__FILE__)), 'pb-ruby')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
+lib_dir = File.join(File.expand_path(File.dirname(__FILE__)), 'lib')
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
 require 'securerandom'
 require 'logging'
 require 'grpc'
 
 require 'janus_services'
+
+require 'generators/mission'
+require 'generators/mission_list'
 
 include Janus
