c00722b2acbc55aa3095e378c449ae71aaa2a73d
Ray Walters <ray@namely.com>
2016-06-20 10:34:13 -0400

    Clean up and document for rubocop

--- pb-ruby/janus.rb
+++ pb-ruby/janus.rb
@@ -4,60 +4,77 @@
 require 'google/protobuf'
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "janus.Error" do
+  add_message 'janus.Error' do
     optional :message, :string, 1
     optional :code, :uint32, 2
-    map :keys, :string, :message, 3, "janus.ErrorMessages"
+    map :keys, :string, :message, 3, 'janus.ErrorMessages'
   end
-  add_message "janus.ErrorMessages" do
+  add_message 'janus.ErrorMessages' do
     repeated :messages, :string, 1
   end
-  add_message "janus.Mission" do
+  add_message 'janus.Mission' do
     optional :id, :int32, 1
     optional :company_name, :string, 2
     optional :client_id, :int32, 3
     optional :mission_name, :string, 4
   end
-  add_message "janus.MissionList" do
-    repeated :missions, :message, 1, "janus.Mission"
+  add_message 'janus.MissionList' do
+    repeated :missions, :message, 1, 'janus.Mission'
   end
-  add_message "janus.CreateMissionReq" do
-    optional :mission, :message, 1, "janus.Mission"
+  add_message 'janus.CreateMissionReq' do
+    optional :mission, :message, 1, 'janus.Mission'
   end
-  add_message "janus.CreateMissionResp" do
+  add_message 'janus.CreateMissionResp' do
     oneof :resp do
-      optional :mission, :message, 1, "janus.Mission"
-      optional :error, :message, 2, "janus.Error"
+      optional :mission, :message, 1, 'janus.Mission'
+      optional :error, :message, 2, 'janus.Error'
     end
   end
-  add_message "janus.GetMissionReq" do
+  add_message 'janus.GetMissionReq' do
     optional :id, :string, 1
   end
-  add_message "janus.GetMissionResp" do
+  add_message 'janus.GetMissionResp' do
     oneof :resp do
-      optional :mission, :message, 1, "janus.Mission"
-      optional :error, :message, 2, "janus.Error"
+      optional :mission, :message, 1, 'janus.Mission'
+      optional :error, :message, 2, 'janus.Error'
     end
   end
-  add_message "janus.GetMissionListReq" do
+  add_message 'janus.GetMissionListReq' do
   end
-  add_message "janus.GetMissionListResp" do
+  add_message 'janus.GetMissionListResp' do
     oneof :resp do
-      optional :mission_list, :message, 1, "janus.MissionList"
-      optional :error, :message, 2, "janus.Error"
+      optional :mission_list, :message, 1, 'janus.MissionList'
+      optional :error, :message, 2, 'janus.Error'
     end
   end
 end
 
+# Namespace for various messages we use
 module Janus
-  Error = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.Error").msgclass
-  ErrorMessages = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.ErrorMessages").msgclass
-  Mission = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.Mission").msgclass
-  MissionList = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.MissionList").msgclass
-  CreateMissionReq = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.CreateMissionReq").msgclass
-  CreateMissionResp = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.CreateMissionResp").msgclass
-  GetMissionReq = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.GetMissionReq").msgclass
-  GetMissionResp = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.GetMissionResp").msgclass
-  GetMissionListReq = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.GetMissionListReq").msgclass
-  GetMissionListResp = Google::Protobuf::DescriptorPool.generated_pool.lookup("janus.GetMissionListResp").msgclass
+  Error =
+    pool.lookup('janus.Error').msgclass
+  ErrorMessages =
+    pool.lookup('janus.ErrorMessages').msgclass
+  Mission =
+    pool.lookup('janus.Mission').msgclass
+  MissionList =
+    pool.lookup('janus.MissionList').msgclass
+  CreateMissionReq =
+    pool.lookup('janus.CreateMissionReq').msgclass
+  CreateMissionResp =
+    pool.lookup('janus.CreateMissionResp').msgclass
+  GetMissionReq =
+    pool.lookup('janus.GetMissionReq').msgclass
+  GetMissionResp =
+    pool.lookup('janus.GetMissionResp').msgclass
+  GetMissionListReq =
+    pool.lookup('janus.GetMissionListReq').msgclass
+  GetMissionListResp =
+    pool.lookup('janus.GetMissionListResp').msgclass
+
+  private
+
+  def pool
+    @pool ||= Google::Protobuf::DescriptorPool.generated_pool
+  end
 end
